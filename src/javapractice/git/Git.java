c5ccc7e7a3fa5b1bdfc5d3706d780d628777e2ce
Siddhant Bhardwaj <67505660+Siddhant791@users.noreply.github.com>
2025-10-26 02:41:01 +0530

    Change printed name from 'Siddhant' to 'New'

--- src/javapractice/git/Git.java
+++ src/javapractice/git/Git.java
@@ -2,7 +2,7 @@ package javapractice.git;
 
 public class Git {
     public void print(){
-        System.out.println("Siddhant");
+        System.out.println("New");
     }
 
     public void siddhant(){
