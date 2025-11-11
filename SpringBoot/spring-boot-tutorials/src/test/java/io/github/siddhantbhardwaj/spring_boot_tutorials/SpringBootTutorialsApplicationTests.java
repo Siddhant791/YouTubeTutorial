631c826eabe8a4b9d0e93eaaf42198e0c2d52326
siddhantbhardwaj <bhardwajsiddhant6@gmail.com>
2025-11-11 17:39:59 +0530

    Adding spring boot project

---
+++ SpringBoot/spring-boot-tutorials/src/test/java/io/github/siddhantbhardwaj/spring_boot_tutorials/SpringBootTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.siddhantbhardwaj.spring_boot_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
